1fa1b225259b46481a46a2062b5f328d7f6f72ff
zhangzhijun <zhangzhijun@58ganji.com>
2018-07-22 17:28:19 +0800

    add user

--- src/router/index.js
+++ src/router/index.js
@@ -45,95 +45,95 @@ export default new Router({
 })
 
 export const asyncRouterMap = [
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree', roles: ['admin'] }
-      }
-    ]
-  },
+  // {
+  //   path: '/example',
+  //   component: Layout,
+  //   redirect: '/example/table',
+  //   name: 'Example',
+  //   meta: { title: 'Example', icon: 'example' },
+  //   children: [
+  //     {
+  //       path: 'table',
+  //       name: 'Table',
+  //       component: () => import('@/views/table/index'),
+  //       meta: { title: 'Table', icon: 'table' }
+  //     },
+  //     {
+  //       path: 'tree',
+  //       name: 'Tree',
+  //       component: () => import('@/views/tree/index'),
+  //       meta: { title: 'Tree', icon: 'tree', roles: ['admin'] }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: '/form',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       name: 'Form',
+  //       component: () => import('@/views/form/index'),
+  //       meta: { title: 'Form', icon: 'form' }
+  //     }
+  //   ]
+  // },
 
   {
-    path: '/form',
+    path: '/user',
     component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'nested',
+    redirect: '/user/list',
+    name: 'user',
     meta: {
-      title: 'nested',
-      icon: 'nested'
+      title: '用户管理',
+      icon: 'user'
     },
     children: [
+      // {
+      //   path: 'menu1',
+      //   component: () => import('@/views/nested/menu1/index'), // Parent router-view
+      //   name: 'menu1',
+      //   meta: { title: 'menu1' },
+      //   children: [
+      //     {
+      //       path: 'menu1-1',
+      //       component: () => import('@/views/nested/menu1/menu1-1'),
+      //       name: 'menu1-1',
+      //       meta: { title: 'menu1-1' }
+      //     },
+      //     {
+      //       path: 'menu1-2',
+      //       component: () => import('@/views/nested/menu1/menu1-2'),
+      //       name: 'menu1-2',
+      //       meta: { title: 'menu1-2' },
+      //       children: [
+      //         {
+      //           path: 'menu1-2-1',
+      //           component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+      //           name: 'menu1-2-1',
+      //           meta: { title: 'menu1-2-1' }
+      //         },
+      //         {
+      //           path: 'menu1-2-2',
+      //           component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+      //           name: 'menu1-2-2',
+      //           meta: { title: 'menu1-2-2' }
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       path: 'menu1-3',
+      //       component: () => import('@/views/nested/menu1/menu1-3'),
+      //       name: 'menu1-3',
+      //       meta: { title: 'menu1-3' }
+      //     }
+      //   ]
+      // },
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'menu1',
-        meta: { title: 'menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'menu1-1',
-            meta: { title: 'menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'menu1-2',
-            meta: { title: 'menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'menu1-2-1',
-                meta: { title: 'menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'menu1-2-2',
-                meta: { title: 'menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'menu1-3',
-            meta: { title: 'menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
+        path: 'list',
+        component: () => import('@/views/user/list'),
+        meta: { title: '用户列表' }
       }
     ]
   },
