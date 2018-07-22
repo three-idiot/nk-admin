a3f34c24be813350e84ad6cfe6c54202050b3a03
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-22 19:30:40 +0800

    'fixing'

--- src/views/visa/list/index.vue
+++ src/views/visa/list/index.vue
@@ -1,38 +1,99 @@
 <template>
   <div class="app-container">
-    <el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column align="center" label='ID' width="95">
+    <!--<el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>-->
+      <!--<el-table-column align="center" label='ID' width="95">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.$index}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Title">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.row.title}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Author" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--<span>{{scope.row.author}}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Pageviews" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.row.pageviews}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column class-name="status-col" label="Status" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column align="center" prop="created_at" label="Display_time" width="200">-->
+        <!--<template slot-scope="scope">-->
+          <!--<i class="el-icon-time"></i>-->
+          <!--<span>{{scope.row.display_time}}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
+    <el-table  :data="list" v-loading="listLoading" border fit highlight-current-row
+              style="width: 100%;min-height:1000px;">
+      <el-table-column align="center"  width="65">
         <template slot-scope="scope">
-          {{scope.$index}}
+          <span>{{scope.row.id}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column width="150px" align="center" >
         <template slot-scope="scope">
-          {{scope.row.title}}
+          <span>{{scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column min-width="150px" >
+        <template slot-scope="scope">
+          <span class="link-type" @click="handleUpdate(scope.row)">{{scope.row.title}}</span>
+          <el-tag>{{scope.row.type | typeFilter}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column width="110px" align="center" >
         <template slot-scope="scope">
           <span>{{scope.row.author}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column width="110px"  align="center" >
         <template slot-scope="scope">
-          {{scope.row.pageviews}}
+          <span style='color:red;'>{{scope.row.reviewer}}</span>
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+      <el-table-column width="80px" >
+        <template slot-scope="scope">
+          <svg-icon v-for="n in +scope.row.importance" icon-class="star" class="meta-item__icon" :key="n"></svg-icon>
+        </template>
+      </el-table-column>
+      <el-table-column align="center"  width="95">
+        <template slot-scope="scope">
+          <span v-if="scope.row.pageviews" class="link-type" @click='handleFetchPv(scope.row.pageviews)'>{{scope.row.pageviews}}</span>
+          <span v-else>0</span>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col"  width="100">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column align="center"  width="230" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span>{{scope.row.display_time}}</span>
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">
+            查看商品
+          </el-button>
+          <el-button v-if="scope.row.status!='published'" size="mini" type="success">
+            修改分成
+          </el-button>
+          <el-button v-if="scope.row.status!='draft'" size="mini" >
+          </el-button>
+          <el-button v-if="scope.row.status!='deleted'" size="mini" type="danger" >
+            违规下架
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
+
   </div>
 </template>
 
