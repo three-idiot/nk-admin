aaa9dee828e4cac96a9d62d5a595976f83a68f8b
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-22 19:56:25 +0800

    'fix'

--- src/views/visa/list/index.vue
+++ src/views/visa/list/index.vue
@@ -35,49 +35,58 @@
     <!--</el-table>-->
     <el-table  :data="list" v-loading="listLoading" border fit highlight-current-row
               style="width: 100%;min-height:1000px;">
-      <el-table-column align="center"  width="65">
+
+      <el-table-column align="center"  width="65" label="序号">
         <template slot-scope="scope">
           <span>{{scope.row.id}}</span>
         </template>
       </el-table-column>
-      <el-table-column width="150px" align="center" >
+
+      <el-table-column width="150px" align="center" label="商品名称">
         <template slot-scope="scope">
           <span>{{scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
         </template>
       </el-table-column>
-      <el-table-column min-width="150px" >
+
+      <el-table-column min-width="150px" label="有效期(天)">
         <template slot-scope="scope">
           <span class="link-type" @click="handleUpdate(scope.row)">{{scope.row.title}}</span>
           <el-tag>{{scope.row.type | typeFilter}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column width="110px" align="center" >
+
+      <el-table-column width="110px" align="center" label="签证价格(惠)">
         <template slot-scope="scope">
           <span>{{scope.row.author}}</span>
         </template>
       </el-table-column>
-      <el-table-column width="110px"  align="center" >
+
+      <el-table-column width="110px"  align="center" label="发布时间">
         <template slot-scope="scope">
           <span style='color:red;'>{{scope.row.reviewer}}</span>
         </template>
       </el-table-column>
-      <el-table-column width="80px" >
-        <template slot-scope="scope">
-          <svg-icon v-for="n in +scope.row.importance" icon-class="star" class="meta-item__icon" :key="n"></svg-icon>
-        </template>
-      </el-table-column>
-      <el-table-column align="center"  width="95">
+
+      <!--<el-table-column width="80px" label="">-->
+        <!--<template slot-scope="scope">-->
+          <!--<svg-icon v-for="n in +scope.row.importance" icon-class="star" class="meta-item__icon" :key="n"></svg-icon>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+
+      <el-table-column align="center"  width="95" label="分成比例">
         <template slot-scope="scope">
           <span v-if="scope.row.pageviews" class="link-type" @click='handleFetchPv(scope.row.pageviews)'>{{scope.row.pageviews}}</span>
           <span v-else>0</span>
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col"  width="100">
+
+      <el-table-column class-name="status-col"  width="100" label="商品状态">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center"  width="340" class-name="small-padding fixed-width">
+
+      <el-table-column align="center"  width="340" class-name="small-padding fixed-width" label="操作">
         <template slot-scope="scope">
           <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">
             查看商品
@@ -92,6 +101,7 @@
           </el-button>
         </template>
       </el-table-column>
+
     </el-table>
 
   </div>
