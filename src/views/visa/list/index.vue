e0b7e4115207519c62a3e58ef330ccca9ed4fd4e
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-24 23:17:37 +0800

    'fi'

--- src/views/visa/list/index.vue
+++ src/views/visa/list/index.vue
@@ -62,10 +62,10 @@
 
       <el-table-column align="center"  width="340" class-name="small-padding fixed-width" label="操作">
         <template slot-scope="scope">
-          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">
+          <el-button  size="mini" type="success">
             查看商品
           </el-button>
-          <el-button  size="mini" type="success">
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">
             修改分成
           </el-button>
           <!--<el-button v-if="scope.row.status!='draft'" size="mini" ></el-button>-->
@@ -80,7 +80,7 @@
 
     <!--分页-->
     <div class="pagination-container" style="margin-top: 30px;">
-      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[10,20,30,50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[4,10,20,30,50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </div>
     <!--分页结束-->
@@ -103,7 +103,7 @@
         },
         listQuery: {
           page: 1,
-          limit: 10,
+          size: 10,
         },
       }
     },
@@ -134,7 +134,7 @@
         })
       },
       handleSizeChange(val) {
-        this.listQuery.limit = val
+        this.listQuery.size = val
         this.fetchData()
       },
       handleCurrentChange(val) {
