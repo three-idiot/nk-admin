63881a4fa75c272103d811e9f3c5a503401f1165
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-25 22:19:48 +0800

    'visaAddPage-reg'

--- src/views/visa/add/index.vue
+++ src/views/visa/add/index.vue
@@ -1,71 +1,74 @@
 <template>
-  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="130px" class="demo-ruleForm">
-    <!--商品名称-->
-    <el-form-item label="商品名称" prop="title" style="width: 312px;">
-      <el-input v-model="ruleForm.title"></el-input>
-    </el-form-item>
-    <!--商品图片-->
+  <div class="addVisa-form">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="130px" class="demo-ruleForm">
+      <!--商品名称-->
+      <el-form-item label="商品名称" prop="title" style="width: 312px;">
+        <el-input v-model="ruleForm.title"></el-input>
+      </el-form-item>
+      <!--商品图片-->
 
-    <!--图片上传-->
+      <!--图片上传-->
 
 
-    <!--签证有效期-->
-    <el-form-item label="签证有效期" prop="unitDay">
-      <el-select v-model="ruleForm.unitDay" placeholder="请选择">
-        <el-option  v-for="(val,key) in unitDay"  :label="val" :value="key"></el-option>
-      </el-select>
-    </el-form-item>
-    <!--入境次数-->
-    <el-form-item label="入境次数" prop="intoType">
-      <el-select v-model="ruleForm.intoType" placeholder="请选择">
-        <el-option  v-for="(val,key) in intoType"  :label="val" :value="key"></el-option>
-      </el-select>
-    </el-form-item>
-    <!--是否加急-->
-    <el-form-item label="是否加急" prop="isUrgent">
-      <el-radio-group v-model="ruleForm.isUrgent">
-        <el-radio v-for="(val,key) in isUrgent" :label="val" :value="key"  ></el-radio>
-      </el-radio-group>
-    </el-form-item>
-    <!--是否面试-->
-    <el-form-item label="是否面试" prop="isInterview">
-      <el-radio-group v-model="ruleForm.isInterview">
-        <el-radio v-for="(val,key) in isInterview" :label="val" :value="key"  ></el-radio>
-      </el-radio-group>
-    </el-form-item>
-    <!--签证费-->
-    <el-form-item label="签证费(元)" prop="visaPrice"  style="width: 312px;">
-      <el-input v-model="ruleForm.visaPrice"></el-input>
-    </el-form-item>
-    <!--签证优惠费-->
-    <el-form-item label="签证优惠费(元)" prop="lowVisaPrice"  style="width: 312px;">
-      <el-input v-model="ruleForm.lowVisaPrice"></el-input>
-    </el-form-item>
-    <!--服务费-->
-    <el-form-item label="服务费(元)" prop="helpPrice"  style="width: 312px;">
-      <el-input v-model="ruleForm.helpPrice"></el-input>
-    </el-form-item>
-    <!--服务优惠费-->
-    <el-form-item label="服务优惠费(元)" prop="lowHelpPrice"  style="width: 312px;">
-      <el-input v-model="ruleForm.lowHelpPrice"></el-input>
-    </el-form-item>
-    <!--停留时间-->
-    <el-form-item label="停留时间(天)" prop="stayDay"  style="width: 312px;">
-      <el-input v-model="ruleForm.stayDay"></el-input>
-    </el-form-item>
-    <!--处理时间-->
-    <el-form-item label="处理时间(天)" prop="disposeDay"  style="width: 312px;">
-      <el-input v-model="ruleForm.disposeDay"></el-input>
-    </el-form-item>
-    <!--续签费用-->
-    <el-form-item label="续签费用(元)" prop="renewPrice"  style="width: 312px;">
-      <el-input v-model="ruleForm.renewPrice"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-      <el-button @click="resetForm('ruleForm')">重置</el-button>
-    </el-form-item>
-  </el-form>
+
+      <!--签证有效期-->
+      <el-form-item label="签证有效期" prop="unitDay">
+        <el-select v-model="ruleForm.unitDay" placeholder="请选择">
+          <el-option  v-for="(val,key) in unitDay"  :label="val" :value="key"></el-option>
+        </el-select>
+      </el-form-item>
+      <!--入境次数-->
+      <el-form-item label="入境次数" prop="intoType">
+        <el-select v-model="ruleForm.intoType" placeholder="请选择">
+          <el-option  v-for="(val,key) in intoType"  :label="val" :value="key"></el-option>
+        </el-select>
+      </el-form-item>
+      <!--是否加急-->
+      <el-form-item label="是否加急" prop="isUrgent">
+        <el-radio-group v-model="ruleForm.isUrgent">
+          <el-radio v-for="(val,key) in isUrgent" :label="val" :value="key"  ></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <!--是否面试-->
+      <el-form-item label="是否面试" prop="isInterview">
+        <el-radio-group v-model="ruleForm.isInterview">
+          <el-radio v-for="(val,key) in isInterview" :label="val" :value="key"  ></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <!--签证费-->
+      <el-form-item label="签证费(元)" prop="visaPrice"  style="width: 312px;">
+        <el-input v-model="ruleForm.visaPrice"></el-input>
+      </el-form-item>
+      <!--签证优惠费-->
+      <el-form-item label="签证优惠费(元)" prop="lowVisaPrice"  style="width: 312px;">
+        <el-input v-model="ruleForm.lowVisaPrice"></el-input>
+      </el-form-item>
+      <!--服务费-->
+      <el-form-item label="服务费(元)" prop="helpPrice"  style="width: 312px;">
+        <el-input v-model="ruleForm.helpPrice"></el-input>
+      </el-form-item>
+      <!--服务优惠费-->
+      <el-form-item label="服务优惠费(元)" prop="lowHelpPrice"  style="width: 312px;">
+        <el-input v-model="ruleForm.lowHelpPrice"></el-input>
+      </el-form-item>
+      <!--停留时间-->
+      <el-form-item label="停留时间(天)" prop="stayDay"  style="width: 312px;">
+        <el-input v-model="ruleForm.stayDay"></el-input>
+      </el-form-item>
+      <!--处理时间-->
+      <el-form-item label="处理时间(天)" prop="disposeDay"  style="width: 312px;">
+        <el-input v-model="ruleForm.disposeDay"></el-input>
+      </el-form-item>
+      <!--续签费用-->
+      <el-form-item label="续签费用(元)" prop="renewPrice"  style="width: 312px;">
+        <el-input v-model="ruleForm.renewPrice"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -107,6 +110,40 @@ export default {
         renewPrice: ''
       },
       rules: {
+        title: [
+          { required: true,trigger: 'blur',message: '请输入商品名称' }
+        ],
+        unitDay :[
+          {required: true,trigger: 'change',message: '请选择签证有效期' }
+        ],
+        intoType: [
+          {required: true,trigger: 'change',message: '请选择入境次数' }
+        ],
+        isUrgent: [
+          {required: true,trigger: 'change',message: '请选择是否加急' }
+        ],
+        isInterview: [
+          {required: true,trigger: 'change',message: '请选择是否面试' }
+        ],
+        visaPrice: [
+          { required: true,trigger: 'blur',message: '请输入签证费' }
+        ],
+        lowVisaPrice: [
+          { required: true,trigger: 'blur',message: '请输入签证优惠费' }
+        ],
+        helpPrice: [
+          { required: true,trigger: 'blur',message: '请输入服务费' }
+        ],
+        lowHelpPrice: [
+          { required: true,trigger: 'blur',message: '请输入服务优惠费' }
+        ],
+        stayDay: [
+          { required: true,trigger: 'blur',message: '请输入停留时间' }
+        ],
+        disposeDay: [
+          { required: true,trigger: 'blur',message: '请输入处理时间' }
+        ],
+        renewPrice:[],
         name: [
           {required: true, message: '请输入活动名称', trigger: 'blur'},
           {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
@@ -185,6 +222,9 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .addVisa-form {
+    margin-top: 20px;
   }
 
 
