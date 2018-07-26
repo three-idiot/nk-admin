2bf603ef9667a67a2a7a092505172473a07e1b80
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-26 17:04:13 +0800

    'fix'

--- src/views/visa/detail/index.vue
+++ src/views/visa/detail/index.vue
@@ -12,22 +12,15 @@
         return {}
       },
       created() {
+        // console.log('吴志鹏', this.$route);
         this.fetchData()
       },
       methods: {
-        getQueryString(name) {
-          let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-          let r = window.location.search.substr(1).match(reg);
-          if (r != null) return decodeURIComponent(r[2]);
-          return null;
-        },
         fetchData() {
-          let id = this.getQueryString('id');
-          console.log('哈哈', id);
+          let id = this.$route.query.id;
           let url = '/gooddetail/' + id;
-          console.log('调试', url);
           getVisaDetail( url ).then( res => {
-            console.log( '调试', res );
+            console.log( res );
           })
         }
       }
