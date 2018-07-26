aff9ff6bdf94688bb1e43326379ca07e1ff83e5f
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-26 18:52:35 +0800

    'fix-detail'

--- src/views/visa/detail/index.vue
+++ src/views/visa/detail/index.vue
@@ -6,6 +6,13 @@
 
 <script>
     import { getVisaDetail } from '@/api/table'
+    import  transformData  from '../tools/data'
+    let goodStatus = transformData.status;
+    let auditStatus = transformData.auditStatus;
+    let unitDay = transformData.unitDay;
+    let intoType = transformData.intoType;
+    let isUrgent = transformData.isUrgent;
+    let isInterview = transformData.isInterview;
     export default {
       name: 'index',
       data() {
@@ -17,9 +24,26 @@
         processData () {
           let arr = [];
           if ( this.data ) {
-            arr.push( {key:'签证名称', value: this.data.title} );
-            arr.push( {key:'签证编码', value: this.data.goodsNum} );
-            arr.push( {key:'签证照片', value: this.data.image[0].goodPath,type: 'image' } );
+            arr.push( {key: '签证名称', value: this.data.title } );
+            arr.push( {key: '签证编码', value: this.data.goodsNum} );
+            arr.push( {key: '签证照片', value: this.data.image[0].goodPath,type: 'image' } );
+            arr.push( {key: '状态', value: goodStatus[this.data.status] } );
+            arr.push( {key: '审核状态', value: auditStatus[this.data.auditStatus] } );
+            arr.push( {key: '签证价格(元)', value: this.data.visaPrice/100 } );
+            arr.push( {key: '签证优惠价格(元)', value: this.data.lowVisaPrice/100 } );
+            arr.push( {key: '服务价格(元)', value: this.data.helpPrice/100 } );
+            arr.push( {key: '服务优惠价格(元)', value: this.data.lowHelpPrice/100 } );
+            arr.push( {key: '续签费用(元)', value: this.data.renewPrice/100 } );
+            arr.push( {key: '签证有效期(天)', value: unitDay[this.data.unitDay] } );
+            arr.push( {key: '停留时间(天)', value: this.data.stayDay} );
+            arr.push( {key: '处理时间(天)', value: this.data.disposeDay} );
+            arr.push( {key: '入境类型', value: intoType[this.data.intoType]} );
+            arr.push( {key: '是否加急', value: isUrgent[this.data.isUrgent]} );
+            arr.push( {key: '是否面试', value: isUrgent[this.data.isInterview]} );
+            arr.push( {key: '旅行社分成(%)', value: this.data.travelRatio } );
+            arr.push( {key: '渠道分成(%)', value: this.data.channelRatio } );
+            arr.push( {key: '平台分成(%)', value: this.data.terraceRatio } );
+            arr.push( {key: '创建时间', value: this.data.ctime } );
             return arr;
           }
 
---
+++ src/views/visa/tools/data.js
@@ -0,0 +1,31 @@
+const transformData = {
+  status: {
+    0: '仓库中',
+    1: '出售中',
+    2: '违规下架'
+  },
+  auditStatus: {
+    0:'审核中',
+    1:'审核通过',
+    2:'审核未通过'
+  },
+  unitDay: {
+    30: '30',
+    60: '60',
+    90: '90',
+    0: '长期',
+  },
+  intoType:{
+    0: '单次',
+    1: '多次'
+  },
+  isUrgent: {
+    1: '是',
+    0: '否',
+  },
+  isInterview: {
+    1: '是',
+    0: '否',
+  },
+};
+export default transformData;
