96d2af287686ad0f0e6b40f1b518bd8754b048a4
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-07-26 18:06:20 +0800

    'fix'

--- src/views/visa/detail/index.vue
+++ src/views/visa/detail/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="visaDetail">
-      <h1>你好呀</h1>
+      <h1>{{ processData }}</h1>
     </div>
 </template>
 
@@ -9,7 +9,21 @@
     export default {
       name: 'index',
       data() {
-        return {}
+        return {
+          data: '',
+        }
+      },
+      computed: {
+        processData () {
+          let arr = [];
+          if ( this.data ) {
+            arr.push( {key:'签证名称', value: this.data.title} );
+            arr.push( {key:'签证编码', value: this.data.goodsNum} );
+            arr.push( {key:'签证照片', value: this.data.image[0].goodPath,type: 'image' } );
+            return arr;
+          }
+
+        }
       },
       created() {
         // console.log('吴志鹏', this.$route);
@@ -20,7 +34,7 @@
           let id = this.$route.query.id;
           let url = '/gooddetail/' + id;
           getVisaDetail( url ).then( res => {
-            console.log( res );
+            this.data = res.data;
           })
         }
       }
