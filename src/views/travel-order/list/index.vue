f885c09754375069891982927adfe850f16700ef
zhangzhijun <zhangzhijun@58ganji.com>
2018-08-07 16:54:24 +0800

    add travel order

--- src/views/travel-order/list/index.vue
+++ src/views/travel-order/list/index.vue
@@ -2,39 +2,39 @@
 <div class="app-container">
     <div style="padding:30px;background:#F2F6FC;">
         <el-form :inline="true" :model="form" class="demo-form-inline">
-            <el-form-item label="订单号">
-                <el-input v-model="form.orderNum" placeholder="请输入订单号"></el-input>
+            <el-form-item label="商品名称">
+                <el-input v-model="form.goodsName" placeholder="请输入订单号"></el-input>
             </el-form-item>
-            <el-form-item label="买家姓名">
-                <el-input v-model="form.linkName" placeholder="请输入姓名"></el-input>
-            </el-form-item>
-            <el-form-item label="订单类型">
-                <el-select v-model="form.renewType" placeholder="请选择" clearable>
-                    <el-option label="正常订单" value="0"></el-option>
-                    <el-option label="续签订单" value="1"></el-option>
+            <el-form-item label="最低成团人数">
+                <el-select class="small-select" v-model="form.peopleMinRule" placeholder="">
+                    <el-option label=">" value="0"></el-option>
+                    <el-option label="=" value="1"></el-option>
+                    <el-option label="<" value="2"></el-option>
                 </el-select>
+                <el-input-number v-model="form.peopleMinNum" :min="1" :max="100" label="最低成团人数"></el-input-number>
+            </el-form-item>
+            <el-form-item label="成团人数上限">
+                <el-select class="small-select" v-model="form.peopleMaxRule" placeholder="">
+                    <el-option label=">" value="0"></el-option>
+                    <el-option label="=" value="1"></el-option>
+                    <el-option label="<" value="2"></el-option>
+                </el-select>
+                <el-input-number v-model="form.peopleMaxNum" :min="1" :max="100" label="成团人数上限"></el-input-number>
             </el-form-item>
             <el-form-item label="订单状态">
-                <el-select v-model="form.status" placeholder="请选择" clearable>
-                    <el-option label="待付款" value="10"></el-option>
-                    <el-option label="办理中" value="20"></el-option>
-                    <el-option label="已送签" value="30"></el-option>
-                    <el-option label="已签发" value="40"></el-option>
-                    <el-option label="已拒签" value="41"></el-option>
-                    <el-option label="已登记" value="50"></el-option>
-                    <el-option label="已过期" value="60"></el-option>
-                    <el-option label="已取消" value="70"></el-option>
+                <el-select v-model="form.status">
+                    <el-option label="全部" value="10"></el-option>
+                    <el-option label="报名中" value="20"></el-option>
+                    <el-option label="已取消" value="30"></el-option>
+                    <el-option label="已成团" value="40"></el-option>
+                    <el-option label="已结束" value="41"></el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="时间类型">
-                <el-select v-model="form.timeType" placeholder="请选择" clearable>
-                    <el-option label="下单时间" value="0"></el-option>
-                    <el-option label="付款时间" value="1"></el-option>
-                    <el-option label="签证时间" value="2"></el-option>
-                    <el-option label="入境时间" value="3"></el-option>
-                </el-select>
+            <el-form-item label="开始时间">
+                <el-date-picker v-model="daterange" type="daterange" value-format="yyyy-MM-dd" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期">
+                </el-date-picker>
             </el-form-item>
-            <el-form-item label="时间">
+            <el-form-item label="结束时间">
                 <el-date-picker v-model="daterange" type="daterange" value-format="yyyy-MM-dd" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期">
                 </el-date-picker>
             </el-form-item>
@@ -47,34 +47,44 @@
     </div>
     <p>订单总数 <span class="red">{{total_count}}</span> 条 支付总额 <span class="red">{{priceCount}}</span> 元</p>
     <el-table :stripe="true" :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>
-        <el-table-column align="center" label='订单编号'>
+        <el-table-column align="center" label='商品编号'>
             <template slot-scope="scope">
                 {{scope.row.goodsNo}}
             </template>
         </el-table-column>
-        <el-table-column align="center" label="下单用户">
+        <el-table-column align="center" label="商品名称">
             <template slot-scope="scope">
-                <!-- {{scope.row.linkName}} -->
+                {{scope.row.goodsName}}
             </template>
         </el-table-column>
-        <el-table-column label="下单时间" align="center">
+        <el-table-column label="商品分类" align="center">
             <template slot-scope="scope">
-                {{new Date(scope.row.ctime).Format("yyyy-MM-dd HH:mm:ss")}}
+                {{scope.row.goodsType}}
             </template>
         </el-table-column>
-        <el-table-column label="付款时间" align="center">
+        <el-table-column label="开始时间" align="center">
             <template slot-scope="scope">
-                {{new Date(scope.row.payTime).Format("yyyy-MM-dd HH:mm:ss")}}
+                {{new Date(scope.row.goodsUpDate).Format("yyyy-MM-dd HH:mm:ss")}}
             </template>
         </el-table-column>
-        <el-table-column label="订单总额" width="110" align="center">
+        <el-table-column label="结束时间" align="center">
             <template slot-scope="scope">
-                {{scope.row.orderMoney}}
+                {{new Date(scope.row.goodsCloseDate).Format("yyyy-MM-dd HH:mm:ss")}}
             </template>
         </el-table-column>
-        <el-table-column class-name="status-col" label="支付方式" width="110" align="center">
+        <el-table-column label="最低成团人数" width="110" align="center">
             <template slot-scope="scope">
-                <el-tag :type="payTypes[scope.row.payType].color">{{payTypes[scope.row.payType].msg}}</el-tag>
+                {{scope.row.peopleMinNum}}
+            </template>
+        </el-table-column>
+        <el-table-column label="成团人数上限" width="110" align="center">
+            <template slot-scope="scope">
+                {{scope.row.peopleMaxNum}}
+            </template>
+        </el-table-column>
+        <el-table-column label="已报人数" width="110" align="center">
+            <template slot-scope="scope">
+                {{scope.row.buyNum}}
             </template>
         </el-table-column>
         <el-table-column class-name="status-col" label="订单状态" width="110" align="center">
@@ -82,15 +92,11 @@
                 <el-tag :type="status[scope.row.status].color">{{status[scope.row.status].msg}}</el-tag>
             </template>
         </el-table-column>
-        <el-table-column class-name="status-col" label="订单类型" width="110" align="center">
-            <template slot-scope="scope">
-                <el-tag :type="renewTypes[scope.row.renewType].color">{{renewTypes[scope.row.renewType].msg}}</el-tag>
-            </template>
-        </el-table-column>
         <el-table-column label="操作" width="160">
             <template slot-scope="scope">
-                <el-button size="mini" type="success" @click="check(scope.$index, scope.row)">查看</el-button>
-                <el-button size="mini" type="primary" @click="edit(scope.$index, scope.row)">编辑</el-button>
+                <el-button size="mini" type="success" @click="check(scope.$index, scope.row)">查看详情</el-button>
+                <el-button size="mini" type="primary" @click="edit(scope.$index, scope.row)">强制成团</el-button>
+                <el-button size="mini" type="primary" @click="edit(scope.$index, scope.row)">退款</el-button>
             </template>
         </el-table-column>
     </el-table>
@@ -105,7 +111,7 @@
 import {
     getOrderList
 } from "@/api/travel-order";
-import orderMap from "@/map/order";
+import orderMap from "@/map/travel-order";
 export default {
     data() {
         return Object.assign({}, orderMap, {
@@ -118,11 +124,10 @@ export default {
             total_count: null,
             priceCount: null,
             form: {
-                orderNum: null,
-                linkName: null,
-                renewType: null,
-                status: null,
-                timeType: null
+                goodsName: null,
+                peopleMinRule: "1",
+                peopleMaxRule: "1",
+                status: '10'
             },
         });
     },
@@ -143,11 +148,11 @@ export default {
         fetchData() {
             this.listLoading = true;
             getOrderList(this.listQuery).then(response => {
-                this.list = response.data.data;
-                this.priceCount = response.data.priceCount;
-                this.total_count = response.data.total_count;
-                this.current_page = response.data.current_page;
-                this.max_page = response.data.max_page;
+                this.list = response.data;
+                // this.priceCount = response.data.priceCount;
+                // this.total_count = response.data.total_count;
+                // this.current_page = response.data.current_page;
+                // this.max_page = response.data.max_page;
                 this.listLoading = false;
             });
         },
@@ -186,6 +191,9 @@ export default {
     .block {
         margin: 50px 0 30px;
         text-align: center;
+    }
+    .small-select {
+        width: 60px;
     }
 }
 </style>
