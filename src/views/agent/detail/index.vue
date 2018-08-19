701e0a2043af2c1b551ff975ab24e931fa997692
haoleiheliyu <448532169@qq.com>
2018-08-19 14:12:29 +0800

    fix

--- src/views/agent/detail/index.vue
+++ src/views/agent/detail/index.vue
@@ -2,7 +2,7 @@
     <div class="visaDetail">
         <p class="title">代理商详情</p>
         <hr/>
-        <!--<portrait-table :data="processData"></portrait-table>-->
+        <portrait-table :data="processData"></portrait-table>
         <div class="btn-container">
             <el-button type="info" @click.native="$router.back()">返回</el-button>
         </div>
@@ -13,11 +13,11 @@
     import { getAgent} from '@/api/agent';
     import transformData from '@/map/agent';
 
-    let goodStatus = transformData.status;
-    let auditStatus = transformData.auditStatus;
-    let unitDay = transformData.unitDay;
-    let intoType = transformData.intoType;
-    let isUrgent = transformData.isUrgent;
+    // let goodStatus = transformData.status;
+    // let auditStatus = transformData.auditStatus;
+    // let unitDay = transformData.unitDay;
+    // let intoType = transformData.intoType;
+    // let isUrgent = transformData.isUrgent;
     // let isInterview = transformData.isInterview;
     import PortraitTable from "@/components/PortraitTable/index.vue";
 
@@ -32,26 +32,24 @@
             processData() {
                 let arr = [];
                 if (this.data) {
-                    arr.push({key: '签证名称', value: this.data.title});
-                    arr.push({key: '签证编码', value: this.data.goodsNum});
-                    arr.push({key: '签证照片', value: this.data.image[0].goodPath, type: 'image'});
-                    arr.push({key: '状态', value: goodStatus[this.data.status]});
-                    arr.push({key: '审核状态', value: auditStatus[this.data.auditStatus]});
-                    arr.push({key: '签证价格(元)', value: this.data.visaPrice / 100});
-                    arr.push({key: '签证优惠价格(元)', value: this.data.lowVisaPrice / 100});
-                    arr.push({key: '服务价格(元)', value: this.data.helpPrice / 100});
-                    arr.push({key: '服务优惠价格(元)', value: this.data.lowHelpPrice / 100});
-                    arr.push({key: '续签费用(元)', value: this.data.renewPrice / 100});
-                    arr.push({key: '签证有效期(天)', value: unitDay[this.data.unitDay]});
-                    arr.push({key: '停留时间(天)', value: this.data.stayDay});
-                    arr.push({key: '处理时间(天)', value: this.data.disposeDay});
-                    arr.push({key: '入境类型', value: intoType[this.data.intoType]});
-                    arr.push({key: '是否加急', value: isUrgent[this.data.isUrgent]});
-                    arr.push({key: '是否面试', value: isUrgent[this.data.isInterview]});
-                    arr.push({key: '旅行社分成(%)', value: this.data.travelRatio});
-                    arr.push({key: '渠道分成(%)', value: this.data.channelRatio});
-                    arr.push({key: '平台分成(%)', value: this.data.terraceRatio});
-                    arr.push({key: '创建时间', value: new Date(this.data.ctime).Format("yyyy-MM-dd HH:mm:ss")});
+                    arr.push({key: '代理商编码', value: this.data.agentNo});
+                    arr.push({key: '代理商名称', value: this.data.name});
+                    arr.push({key: '代理商属性', value: this.data.roleId});
+                    arr.push({key: '代理商地区', value: goodStatus[this.data.status]});
+                    arr.push({key: '联系人', value: auditStatus[this.data.auditStatus]});
+                    arr.push({key: '联系电话', value: this.data.visaPrice / 100});
+                    arr.push({key: '联系地址', value: this.data.lowVisaPrice / 100});
+                    arr.push({key: '代理商性质', value: this.data.helpPrice / 100});
+                    arr.push({key: '营业执照号码', value: this.data.lowHelpPrice / 100});
+                    arr.push({key: '营业执照副本', value: this.data.renewPrice / 100});
+                    arr.push({key: '有效期至', value: unitDay[this.data.unitDay]});
+                    arr.push({key: '代理标识码', value: this.data.stayDay});
+                    arr.push({key: '代理二维码', value: this.data.disposeDay});
+                    arr.push({key: '创建人', value: intoType[this.data.intoType]});
+                    arr.push({key: '创建时间', value: isUrgent[this.data.isUrgent]});
+                    arr.push({key: '审核人', value: isUrgent[this.data.isInterview]});
+                    arr.push({key: '审核时间', value: this.data.travelRatio});
+                    // arr.push({key: '创建时间', value: new Date(this.data.ctime).Format("yyyy-MM-dd HH:mm:ss")});
                     return arr;
                 }
             }
