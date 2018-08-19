a4b20fc23317453db047e2757c897aa53266ad9e
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-08-19 16:01:52 +0800

    fix

--- src/views/goods/detail/index.vue
+++ src/views/goods/detail/index.vue
@@ -32,26 +32,28 @@
             processData() {
                 let arr = [];
                 if (this.data) {
-                    arr.push({key: '签证名称', value: this.data.title});
-                    arr.push({key: '签证编码', value: this.data.goodsNum});
-                    arr.push({key: '签证照片', value: this.data.image[0].goodPath, type: 'image'});
-                    arr.push({key: '状态', value: goodStatus[this.data.status]});
-                    arr.push({key: '审核状态', value: auditStatus[this.data.auditStatus]});
-                    arr.push({key: '签证价格(元)', value: this.data.visaPrice / 100});
-                    arr.push({key: '签证优惠价格(元)', value: this.data.lowVisaPrice / 100});
-                    arr.push({key: '服务价格(元)', value: this.data.helpPrice / 100});
-                    arr.push({key: '服务优惠价格(元)', value: this.data.lowHelpPrice / 100});
-                    arr.push({key: '续签费用(元)', value: this.data.renewPrice / 100});
-                    arr.push({key: '签证有效期(天)', value: unitDay[this.data.unitDay]});
-                    arr.push({key: '停留时间(天)', value: this.data.stayDay});
-                    arr.push({key: '处理时间(天)', value: this.data.disposeDay});
-                    arr.push({key: '入境类型', value: intoType[this.data.intoType]});
-                    arr.push({key: '是否加急', value: isUrgent[this.data.isUrgent]});
-                    arr.push({key: '是否面试', value: isUrgent[this.data.isInterview]});
-                    arr.push({key: '旅行社分成(%)', value: this.data.travelRatio});
-                    arr.push({key: '渠道分成(%)', value: this.data.channelRatio});
-                    arr.push({key: '平台分成(%)', value: this.data.terraceRatio});
+                    arr.push({key: '商品编号', value: this.data.title});
+                    arr.push({key: '商品状态', value: this.data.goodsNum});
+                    arr.push({key: '商品名称', value: this.data.image[0].goodPath, type: 'image'});
+                    arr.push({key: '商品概要', value: goodStatus[this.data.status]});
+                    arr.push({key: '商品分类', value: auditStatus[this.data.auditStatus]});
+                    arr.push({key: '出发日期和地点', value: this.data.visaPrice / 100});
+                    arr.push({key: '添加图片', value: this.data.lowVisaPrice / 100});
+                    arr.push({key: '线路特色', value: this.data.helpPrice / 100});
+                    arr.push({key: '行程介绍', value: this.data.lowHelpPrice / 100});
+                    arr.push({key: '费用与须知', value: this.data.renewPrice / 100});
+                    arr.push({key: '报名截至日期', value: unitDay[this.data.unitDay]});
+                    arr.push({key: '最低成团人数', value: this.data.stayDay});
+                    arr.push({key: '成团人数上限', value: this.data.disposeDay});
+                    arr.push({key: '商品价格', value: intoType[this.data.intoType]});
+                    arr.push({key: '门市价格', value: isUrgent[this.data.isUrgent]});
+                    arr.push({key: '儿童价格', value: isUrgent[this.data.isInterview]});
+                    arr.push({key: '成人', value: this.data.travelRatio});
+                    arr.push({key: '儿童', value: this.data.channelRatio});
+                    arr.push({key: '创建人', value: this.data.terraceRatio});
                     arr.push({key: '创建时间', value: new Date(this.data.ctime).Format("yyyy-MM-dd HH:mm:ss")});
+                    arr.push({key: '审核人', value: this.data.terraceRatio});
+                    arr.push({key: '审核时间', value: new Date(this.data.ctime).Format("yyyy-MM-dd HH:mm:ss")});
                     return arr;
                 }
             }
