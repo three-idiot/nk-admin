dcfe7535f762205804f582ca9a7361ee88d2032a
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-08-20 20:58:10 +0800

    fix

--- src/views/agent/add/index.vue
+++ src/views/agent/add/index.vue
@@ -388,6 +388,7 @@ export default {
                         console.log( res );
                         if ( res.code == 200 ) {
                             alert('新建成功');
+                            history.back();
                         }
                     })
                 } else {
@@ -415,17 +416,6 @@ export default {
         },
         imgRemove(files, fileList) {
             this.form.bizLicenseImagePath = null;
-        },
-        beforeAvatarUpload(file) {
-            // const isJPG = file.type === 'image/jpeg';
-            const isLt2M = file.size / 1024 / 1024 < 2;
-            // if (!isJPG) {
-            //   this.$message.error('上传头像图片只能是 JPG 格式!');
-            // }
-            if (!isLt2M) {
-                this.$message.error('上传头像图片大小不能超过 2MB!');
-            }
-            return isLt2M;
         },
         update(params) {
             updateOrder(params).then(response => {
