7739b60eea9b0dfb88da64c7ebd2a8ffe7b3ada2
haoleiheliyu <448532169@qq.com>
2018-09-11 00:05:08 +0800

    fix

--- src/views/goods/add/index.vue
+++ src/views/goods/add/index.vue
@@ -48,7 +48,7 @@
                 <el-upload list-type="picture" class="upload-demo" action='/image/uploadfile' name='file' :limit="5"
                            :on-success="imgUploaded" :on-remove="imgRemove">
                     <el-button size="small" type="primary">点击上传</el-button>
-                    <div slot="tip" class="el-upload__tip">如需更换图片，请点击图片右上角删除后重新上传</div>
+                    <div slot="tip" class="el-upload__tip">图片最多只能添加5张！！！</div>
                 </el-upload>
             </el-form-item>
 
