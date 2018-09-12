e9c6e5275c30a32a5273b4f7d29ef002d3cfd6ef
haoleiheliyu <448532169@qq.com>
2018-09-13 00:56:19 +0800

    fix

--- src/views/agent/detail/index.vue
+++ src/views/agent/detail/index.vue
@@ -33,21 +33,21 @@
             processData() {
                 let arr = [];
                 if (this.data) {
-                    arr.push({key: '代理商编码', value: this.data.agentNo});
-                    arr.push({key: '代理商名称', value: this.data.agentName});
-                    arr.push({key: '代理商属性', value: transformData.roleId[this.data.roleId]});
-                    arr.push({key: '代理商地区', value: this.getProvince(this.data.province)});
-                    arr.push({key: '联系人', value: this.data.contactsName});
-                    arr.push({key: '联系电话', value: this.data.contactsPhone});
-                    arr.push({key: '联系地址', value: this.data.contactsMail});
-                    arr.push({key: '代理商性质', value: transformData.type[this.data.agentType]});
-                    arr.push({key: '营业执照号码', value: this.data.bizLicenseCode});
+                    arr.push({key: '代理商编码', value: this.data.agentNo, type: 'string'});
+                    arr.push({key: '代理商名称', value: this.data.agentName, type: 'string'});
+                    arr.push({key: '代理商属性', value: transformData.roleId[this.data.roleId], type: 'string'});
+                    arr.push({key: '代理商地区', value: this.getProvince(this.data.province), type: 'string'});
+                    arr.push({key: '联系人', value: this.data.contactsName, type: 'string'});
+                    arr.push({key: '联系电话', value: this.data.contactsPhone, type: 'string'});
+                    arr.push({key: '联系地址', value: this.data.contactsMail, type: 'string'});
+                    arr.push({key: '代理商性质', value: transformData.type[this.data.agentType], type: 'string'});
+                    arr.push({key: '营业执照号码', value: this.data.bizLicenseCode, type: 'string'});
                     arr.push({key: '营业执照副本', value: this.data.bizLicenseImagePath, type: 'image'});
-                    arr.push({key: '有效期至', value: this.data.expireTime});
-                    arr.push({key: '代理标识码', value: this.data.uniqueCode});
+                    arr.push({key: '有效期至', value: this.data.expireTime, type: 'string'});
+                    arr.push({key: '代理标识码', value: this.data.uniqueCode, type: 'string'});
                     // arr.push({key: '代理二维码', value: this.data.disposeDay});
-                    arr.push({key: '创建人', value: this.data.createUser});
-                    arr.push({key: '创建时间', value: this.data.auditTime});
+                    arr.push({key: '创建人', value: this.data.createUser, type: 'string'});
+                    arr.push({key: '创建时间', value: this.data.auditTime, type: 'string'});
                     // arr.push({key: '审核人', value: this.data.auditUser});
                     // arr.push({key: '审核时间', value: this.data.auditTime});
                     // arr.push({key: '创建时间', value: new Date(this.data.ctime).Format("yyyy-MM-dd HH:mm:ss")});
