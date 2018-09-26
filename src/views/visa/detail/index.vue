01e1982e2ed4af4485d0a0dd64b166fd74216f94
wuzhipeng03 <wuzhipeng03@58ganji.com>
2018-09-26 20:57:41 +0800

    fix

--- src/views/visa/detail/index.vue
+++ src/views/visa/detail/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="visaDetail">
-        <p class="title">商品详情</p>
+        <p class="title">签证详情</p>
         <hr/>
         <portrait-table :data="processData"></portrait-table>
         <div class="btn-container">
@@ -25,35 +25,36 @@
       data() {
         return {
           data: '',
+          processData: [],
         };
       },
       computed: {
-        processData () {
-          let arr = [];
-          if ( this.data ) {
-            arr.push( {key: '签证名称', value: this.data.title});
-            arr.push( {key: '签证编码', value: this.data.goodsNum} );
-            arr.push( {key: '签证照片', value: this.data.image[0].goodPath, type: 'image' } );
-            arr.push( {key: '状态', value: goodStatus[this.data.status] } );
-            arr.push( {key: '审核状态', value: auditStatus[this.data.auditStatus] } );
-            arr.push( {key: '签证价格(元)', value: this.data.visaPrice/100 } );
-            arr.push( {key: '签证优惠价格(元)', value: this.data.lowVisaPrice/100 } );
-            arr.push( {key: '服务价格(元)', value: this.data.helpPrice/100 } );
-            arr.push( {key: '服务优惠价格(元)', value: this.data.lowHelpPrice/100 } );
-            arr.push( {key: '续签费用(元)', value: this.data.renewPrice/100 } );
-            arr.push( {key: '签证有效期(天)', value: unitDay[this.data.unitDay] } );
-            arr.push( {key: '停留时间(天)', value: this.data.stayDay} );
-            arr.push( {key: '处理时间(天)', value: this.data.disposeDay} );
-            arr.push( {key: '入境类型', value: intoType[this.data.intoType]} );
-            arr.push( {key: '是否加急', value: isUrgent[this.data.isUrgent]} );
-            arr.push( {key: '是否面试', value: isUrgent[this.data.isInterview]} );
-            arr.push( {key: '旅行社分成(%)', value: this.data.travelRatio } );
-            arr.push( {key: '渠道分成(%)', value: this.data.channelRatio } );
-            arr.push( {key: '平台分成(%)', value: this.data.terraceRatio } );
-            arr.push( {key: '创建时间', value: new Date(this.data.ctime.replace(/-/g,"/")).Format("yyyy-MM-dd HH:mm:ss") } );
-            return arr;
-          }
-        }
+        // processData () {
+        //   let arr = [];
+        //   if ( this.data ) {
+        //     arr.push( {key: '签证名称', value: this.data.title});
+        //     arr.push( {key: '签证编码', value: this.data.goodsNum} );
+        //     arr.push( {key: '签证照片', value: this.data.image[0].goodPath, type: 'image' } );
+        //     arr.push( {key: '状态', value: goodStatus[this.data.status] } );
+        //     arr.push( {key: '审核状态', value: auditStatus[this.data.auditStatus] } );
+        //     arr.push( {key: '签证价格(元)', value: this.data.visaPrice/100 } );
+        //     arr.push( {key: '签证优惠价格(元)', value: this.data.lowVisaPrice/100 } );
+        //     arr.push( {key: '服务价格(元)', value: this.data.helpPrice/100 } );
+        //     arr.push( {key: '服务优惠价格(元)', value: this.data.lowHelpPrice/100 } );
+        //     arr.push( {key: '续签费用(元)', value: this.data.renewPrice/100 } );
+        //     arr.push( {key: '签证有效期(天)', value: unitDay[this.data.unitDay] } );
+        //     arr.push( {key: '停留时间(天)', value: this.data.stayDay} );
+        //     arr.push( {key: '处理时间(天)', value: this.data.disposeDay} );
+        //     arr.push( {key: '入境类型', value: intoType[this.data.intoType]} );
+        //     arr.push( {key: '是否加急', value: isUrgent[this.data.isUrgent]} );
+        //     arr.push( {key: '是否面试', value: isUrgent[this.data.isInterview]} );
+        //     arr.push( {key: '旅行社分成(%)', value: this.data.travelRatio } );
+        //     arr.push( {key: '渠道分成(%)', value: this.data.channelRatio } );
+        //     arr.push( {key: '平台分成(%)', value: this.data.terraceRatio } );
+        //     arr.push( {key: '创建时间', value: new Date(this.data.ctime.replace(/-/g,"/")).Format("yyyy-MM-dd HH:mm:ss") } );
+        //     return arr;
+        //   }
+        // }
       },
       created() {
         // console.log('吴志鹏', this.$route);
@@ -68,6 +69,28 @@
           let url = '/gooddetail/' + id;
           getVisaDetail( url ).then( res => {
             this.data = res.data;
+            this.processData = [
+                {key: '签证名称', value: this.data.title, type: 'string'},
+                {key: '签证名称', value: this.data.title, type: 'string'},
+                {key: '签证照片', value: this.data.image.length? this.data.image[0].goodPath : '', type: 'image' },
+                {key: '状态', value: goodStatus[this.data.status],  type: 'string' },
+                {key: '审核状态', value: auditStatus[this.data.auditStatus], type: 'string' },
+                {key: '签证价格(元)', value: this.data.visaPrice/100, type: 'string' },
+                {key: '签证优惠价格(元)', value: this.data.lowVisaPrice/100, type: 'string' },
+                {key: '服务价格(元)', value: this.data.helpPrice/100, type: 'string' },
+                {key: '服务优惠价格(元)', value: this.data.lowHelpPrice/100 , type: 'string'},
+                {key: '续签费用(元)', value: this.data.renewPrice/100, type: 'string' },
+                {key: '签证有效期(天)', value: unitDay[this.data.unitDay], type: 'string' },
+                {key: '停留时间(天)', value: this.data.stayDay, type: 'string'},
+                {key: '处理时间(天)', value: this.data.disposeDay, type: 'string'},
+                {key: '入境类型', value: intoType[this.data.intoType], type: 'string'},
+                {key: '是否加急', value: isUrgent[this.data.isUrgent], type: 'string'},
+                {key: '是否面试', value: isUrgent[this.data.isInterview], type: 'string'},
+                {key: '旅行社分成(%)', value: this.data.travelRatio, type: 'string'},
+                {key: '渠道分成(%)', value: this.data.channelRatio, type: 'string' },
+                {key: '平台分成(%)', value: this.data.terraceRatio, type: 'string'},
+                {key: '创建时间', value: new Date(this.data.ctime.replace(/-/g,"/")).Format("yyyy-MM-dd HH:mm:ss"), type: 'string' }
+            ]
           });
         }
       }
