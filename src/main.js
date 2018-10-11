641118dd5106d95ac33fca546b1a98e5ba222ffe
tanghaibo <tanghaibo@58ganji.com>
2018-10-11 23:24:46 +0800

    remove English lang

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import 'normalize.css/normalize.css';// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en'; // lang i18n
 
 import '@/styles/index.scss'; // global css
 
@@ -25,7 +24,7 @@ import permission from '@/directive/permission/index.js';
 import VueQrcode from '@xkeshi/vue-qrcode';
 Vue.component(VueQrcode.name, VueQrcode);
 
-Vue.use(ElementUI, { locale });
+Vue.use(ElementUI);
 Vue.use(permission);
 Vue.use(Plugins);
 
